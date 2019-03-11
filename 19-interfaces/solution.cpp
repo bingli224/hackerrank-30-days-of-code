4b1057ec734518a306ec95ee3a4ba3d0818c85cc
BingLi224 <40303168+bingli224@users.noreply.github.com>
2019-03-11 17:49:58 +0700

    Create solution.cpp

---
+++ 19-interfaces/solution.cpp
@@ -0,0 +1,33 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+class AdvancedArithmetic{
+    public:
+        virtual int divisorSum(int n)=0;
+};
+class Calculator : public AdvancedArithmetic {
+public:
+    int divisorSum(int n) {
+        int divisor = 1, sum = n;
+        while ( divisor < n ) {
+            if ( n % divisor == 0 ) {
+                sum += divisor;
+            }
+            divisor ++;
+        }
+        return sum;
+    }
+};
+
+int main(){
+    int n;
+    cin >> n;
+    AdvancedArithmetic *myCalculator = new Calculator(); 
+    int sum = myCalculator->divisorSum(n);
+    cout << "I implemented: AdvancedArithmetic\n" << sum;
+    return 0;
+}
