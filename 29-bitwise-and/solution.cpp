bb6309f38a6efb1def838e0f11d0edd2feabfb5b
BingLi224 <40303168+bingli224@users.noreply.github.com>
2019-03-16 10:44:51 +0700

    Create solution.cpp

---
+++ 29-bitwise-and/solution.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+
+
+int main()
+{
+    int t;
+    cin >> t;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        string nk_temp;
+        getline(cin, nk_temp);
+
+        vector<string> nk = split_string(nk_temp);
+
+        int n = stoi(nk[0]);
+
+        int k = stoi(nk[1]);
+
+        /**
+         * By BingLi224
+         * 10:43 THA 16/03/2019
+         */
+        int a = 1;
+        int max = 0;
+        while ( a <= n ) {
+            int b = a + 1;
+            while ( b <= n ) {
+                int result = a & b;
+                if ( result < k && result > max )
+                    max = result;
+
+                b ++;
+            }
+            a ++;
+        }
+
+        printf ( "%d\n", max );
+    }
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
